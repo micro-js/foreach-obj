ac4cff1dbe9b50a85e03305f1bad3f830458b6fd
Andrew Shaffer <andrew@weo.io>
2016-01-07 11:42:30 -0800

    add test

--- test/index.js
+++ test/index.js
@@ -2,13 +2,18 @@
  * Imports
  */
 
-var foreachObj = require('..')
+var forEach = require('..')
 var test = require('tape')
 
 /**
  * Tests
  */
 
-test('should work', function (t) {
+ test('should work', function (t) {
+   t.plan(2)
 
-})
+   forEach(function (val, key) {
+     t.equal(val, 2)
+     t.equal(key, 'a')
+   }, {a: 2})
+ })
